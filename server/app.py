c1104fac7dc609cf9604d906a4bd06675b98645e
fgypas <fgypas@gmail.com>
2019-08-03 10:32:29 +0300

    Connection of flask with mongo db using PyMongo and flask_pymongo

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/vzflow"
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello_world():
-    return '<html><h1>Welcome to the development server of vzflow</h1></html>'
+def index():
+    db_info = mongo.db
+    return f"<html>\
+             <h1>Welcome to the development server of vzflow. </h1> <br> \
+             If you can see this message then your DB is set properly <br> \
+             DB info: {db_info}. <br> \
+             </html>"
