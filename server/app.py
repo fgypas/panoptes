46f08b9a5e878adf815cd165138c0b0b7bdafcb7
gkostoulas <gkostoulas@oseven.io>
2019-08-25 23:37:48 +0300

    DB setup and connection with flask

--- server/app.py
+++ server/app.py
@@ -1,19 +1,20 @@
-from flask import Flask, request, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, request, render_template, abort
+from server.database import init_db, db_session
 import traceback
+from server.models import Workflows, WorkflowMessages
+from server.schema_forms import SnakemakeUpdateForm
 
 app = Flask(__name__, template_folder=".")
-app.config["MONGO_URI"] = "mongodb://localhost:27017/vzflow"
-mongo = PyMongo(app, connect=True)
+init_db()
 
 
 @app.route('/')
 def index():
-    db_info = mongo.db
+
     return f"<html> \
              <h1>Welcome to the development server of vzflow. </h1> <br> \
              If you can see this message then your DB is set properly <br> \
-             DB info: {db_info}. <br> \
+             DB info: {Workflows.query.all()}. <br> \
              To check the status of the workflow go to the following route: <a href=\"/workflow_status\">/workflow_status</a> <br> \
              </html>"
 
@@ -21,8 +22,12 @@ def index():
 @app.route('/workflow_status/<id>', methods=['GET'])
 def get_status(id):
     try:
-        doc = mongo.db.vzflow.status.getOne()
-        return str(doc)
+        workflow = Workflows.query.filter(Workflows.id == id).first()
+        if workflow:
+            return str(workflow.name)
+        else:
+            return f"<html>No workflow currently running with id= {id}!!!</html>"
+
     except:
         traceback.print_exc()
         return f"<html>No workflow currently running with id= {id}!!!</html>"
@@ -30,13 +35,20 @@ def get_status(id):
 
 @app.route('/update_workflow_status', methods=['POST', 'GET'])
 def update_status():
-    status = request.form["status"]
+    update_form = SnakemakeUpdateForm()
+    errors = update_form.validate(request.form)
+    if errors:
+        abort(404, str(errors))
+    # now all required fields exist and are the right type
+    # business requirements aren't necessarily satisfied (length, time bounds, etc)
 
-    doc = mongo.db.vzflow.insert({"status": status})
+    w = WorkflowMessages(update_form)
+    db_session.add(w)
+    db_session.commit()
 
-    print(doc)
+    print('New update from snakemake {}'.format(id))
 
-    return "asd"
+    return "ok"
 
 
 if __name__ == '__main__':
